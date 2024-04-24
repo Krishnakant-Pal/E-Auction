f9ff3b09a2f25c9b56a1445c9c1c9dd7966b0de9
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2024-04-24 10:21:29 +0530

    updated listing added imagalink column

--- auctions/models.py
+++ auctions/models.py
@@ -14,29 +14,32 @@ CATAGORIES_CHOICES = {
 
 }
 class Listing(models.Model):
-    
+    owner = models.CharField(max_length=64)
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=248)
     ask_price = models.IntegerField()
     category  = models.CharField(max_length=5,choices=CATAGORIES_CHOICES,null=True)
     creation_date = models.DateTimeField(auto_now=True)
+    imagelink = models.CharField(max_length=128,default=False)
 
     def __str__(self):
-        return f"Title: {self.title},Ask_Price: {self.ask_price},Category: {self.category},Creation_Date: {self.creation_date}"
+        return f"Owner:{self.owner},Title: {self.title},Ask_Price: {self.ask_price},Category: {self.category},Creation_Date: {self.creation_date}"
 
 class Bidding(models.Model):
+    user = models.CharField(max_length=64)
     bid_price = models.IntegerField()
     number_of_item = models.IntegerField()
     listing = models.ForeignKey(Listing,on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Bid Price {self.bid_price},No. of items:{self.number_of_item},listing: {self.listing}"
+        return f"User:{self.user} ,Bid Price {self.bid_price},No. of items:{self.number_of_item},listing: {self.listing}"
 
 class Comment(models.Model):
+    user = models.CharField(max_length=64)
     listing = models.ForeignKey(Listing,on_delete=models.CASCADE)
     comment = models.CharField(max_length=248)
 
     def __str__(self):
-        return f"listing: {self.listing},Comment: {self.comment}"
+        return f"user:{self.user},listing: {self.listing},Comment: {self.comment}"
 
 
